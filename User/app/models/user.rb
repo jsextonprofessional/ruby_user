820cef346c9a8c3c7c38887af567188cf7d988d1
Jacob Sexton <jshuman@protonmail.com>
2021-08-18 20:23:03 +0000

    All tests pass

--- User/app/models/user.rb
+++ User/app/models/user.rb
@@ -6,4 +6,10 @@ class User < ActiveRecord::Base
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :name, :email, presence: true
   validates :email, :uniqueness => true, format: { with: EMAIL_REGEX }
+  
+  before_save :lowercaser
+
+  def lowercaser
+    email.downcase!
+  end
 end
